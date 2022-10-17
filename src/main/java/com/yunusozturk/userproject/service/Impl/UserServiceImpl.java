3a393d38f8b087f007c5abe9b1519d379c010a1c
Yunus.Ozturk <yunus.ozturk@anadoluefes.com>
2022-10-17 15:39:06 +0300

    conflict

--- src/main/java/com/yunusozturk/userproject/service/Impl/UserServiceImpl.java
+++ src/main/java/com/yunusozturk/userproject/service/Impl/UserServiceImpl.java
@@ -15,6 +15,7 @@ public class UserServiceImpl implements UserService {
 
         //Qeue basılacak
         //
+        System.out.println("Adam Yunus");
          userRepository.save(userDto);
 
          return  true;
