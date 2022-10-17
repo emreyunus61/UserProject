c7a17b491f075b25416ed7637268c71943ef769f
Yunus.Ozturk <yunus.ozturk@anadoluefes.com>
2022-10-17 14:36:47 +0300

    fixed

--- src/main/java/com/yunusozturk/userproject/controller/UserController.java
+++ src/main/java/com/yunusozturk/userproject/controller/UserController.java
@@ -4,6 +4,7 @@ import com.yunusozturk.userproject.dto.UserDto;
 import com.yunusozturk.userproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @PostMapping("/save")
     public ResponseEntity<Boolean> saveUser(@RequestBody UserDto userDto){
 
         Boolean response = null;
